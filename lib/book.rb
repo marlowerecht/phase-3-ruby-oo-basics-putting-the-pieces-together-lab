ead4764b14f91c409ec68af2519019e50b8e31a7
Marlowe Recht <recht.marlowe@gmail.com>
2023-01-30 22:15:19 +0100

    commit tree accec8700385ccd3496a874c61bf4c2f47ffe35d

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
+class Book
 
+    attr_reader :title, :author, :page_count, :genre
+    attr_writer :title
+
+    def initialize(title)
+        @title = title
+    end
+
+    def author=(author)
+        @author = author
+    end
+
+    def page_count=(page_count)
+        @page_count = page_count
+    end
+
+    def genre=(genre)
+        @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,36 @@
 # Make your shoe class here!
+require 'pry'
+
+class Shoe
+
+    attr_reader :brand, :color, :size, :material, :condition
+    attr_writer :condition
+
+    def initialize(brand)
+       @brand = brand 
+    end
+
+    def color=(color)
+        @color = color
+    end
+
+    def size=(size)
+        @size = size
+    end
+
+    def material=(material)
+        @material = material
+    end
+
+    def condition=(condition)
+        @condition = condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        self.condition = "new"
+    end
+
+end
+
+# mine = Shoe.new("vans")
